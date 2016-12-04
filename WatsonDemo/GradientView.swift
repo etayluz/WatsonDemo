cb223631033ee70f703d3a283ef4ecdb9f98c06d
Etay Luz <etayluz@gmail.com>
2016-12-04 04:58:05 -0600

    Save work

---
+++ WatsonDemo/GradientView.swift
@@ -0,0 +1,29 @@
+//
+//  GradientView.swift
+//  WatsonDemo
+//
+//  Created by Etay Luz on 12/4/16.
+//  Copyright © 2016 Etay Luz. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+@IBDesignable final class GradientView: UIView {
+
+    @IBInspectable var startColor: UIColor = UIColor.clear
+    @IBInspectable var endColor: UIColor = UIColor.clear
+
+    override func draw(_ rect: CGRect) {
+
+        let gradient:CAGradientLayer = CAGradientLayer()
+        gradient.frame = CGRect(x: CGFloat(0),
+                                y: CGFloat(0),
+                                width: superview!.frame.size.width,
+                                height: superview!.frame.size.height)
+        print(frame.size.height)
+        gradient.colors = [startColor.cgColor, endColor.cgColor]
+        layer.addSublayer(gradient)
+    }
+
+}
