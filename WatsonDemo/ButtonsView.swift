20ec526662533c575a52b15fb85258b25f8f9229
dennisnotojr <dennisnoto@densmac.domain>
2016-12-12 14:14:46 -0700

    changes to  set message label size and button size via device

--- WatsonDemo/ButtonsView.swift
+++ WatsonDemo/ButtonsView.swift
@@ -42,6 +42,68 @@ class ButtonsView: UIView {
         let optionButton = UIButton(frame: CGRect(x: xOffset, y: yOffset, width: 0, height: 0))
         optionButton.backgroundColor = UIColor.buttonBackgroundColor()
         optionButton.titleLabel?.font = UIFont.systemFont(ofSize: 15)
+        
+        var systemInfo = utsname()
+        uname(&systemInfo)
+        let machineMirror = Mirror(reflecting: systemInfo.machine)
+        let identifier = machineMirror.children.reduce("") { identifier, element in
+            guard let value = element.value as? Int8, value != 0 else { return identifier }
+            return identifier + String(UnicodeScalar(UInt8(value)))
+        }
+        switch identifier {
+        //iPhone 4
+        case "iPhone3,1", "iPhone3,2", "iPhone3,3": optionButton.titleLabel?.font = UIFont.systemFont(ofSize: 14);
+        //iPhone 4s
+        case "iPhone4,1": optionButton.titleLabel?.font = UIFont.systemFont(ofSize: 14);
+        //iPhone 5
+        case "iPhone5,1", "iPhone5,2": optionButton.titleLabel?.font = UIFont.systemFont(ofSize: 15);
+        //iPhone 5c
+        case "iPhone5,3", "iPhone5,4": optionButton.titleLabel?.font = UIFont.systemFont(ofSize: 15);
+        //iPhone 5s
+        case "iPhone6,1", "iPhone6,2": optionButton.titleLabel?.font = UIFont.systemFont(ofSize: 15);
+        //iPhone 6
+        case "iPhone7,2": optionButton.titleLabel?.font = UIFont.systemFont(ofSize: 15);
+        //iPhone 6 Plus
+        case "iPhone7,1": optionButton.titleLabel?.font = UIFont.systemFont(ofSize: 15);
+        //iPhone 6s
+        case "iPhone8,1":  optionButton.titleLabel?.font = UIFont.systemFont(ofSize: 15);
+        //iPhone 6s Plus
+        case "iPhone8,2":  optionButton.titleLabel?.font = UIFont.systemFont(ofSize: 17);
+        //iphone 7
+        case "iPhone9,1", "iPhone9,3":  optionButton.titleLabel?.font = UIFont.systemFont(ofSize: 15);
+        //iphone 7 plus
+        case "iPhone9,2", "iPhone9,4":  optionButton.titleLabel?.font = UIFont.systemFont(ofSize: 17);
+         //iphone 7 SE
+        case "iPhone8,4":  optionButton.titleLabel?.font = UIFont.systemFont(ofSize: 15);
+        //iPad2
+        case "iPad2,1", "iPad2,2", "iPad2,3", "iPad2,4":optionButton.titleLabel?.font = UIFont.systemFont(ofSize: 20);
+        //iPad 3
+        case "iPad3,1", "iPad3,2", "iPad3,3": optionButton.titleLabel?.font = UIFont.systemFont(ofSize: 21);
+        //iPad 4
+        case "iPad3,4", "iPad3,5", "iPad3,6":optionButton.titleLabel?.font = UIFont.systemFont(ofSize: 21);
+        //iPad Air
+        case "iPad4,1", "iPad4,2", "iPad4,3":optionButton.titleLabel?.font = UIFont.systemFont(ofSize: 21);
+        //iPad Air 2
+        case "iPad5,3", "iPad5,4": optionButton.titleLabel?.font = UIFont.systemFont(ofSize: 20);
+        //iPad Mini
+        case "iPad2,5", "iPad2,6", "iPad2,7": optionButton.titleLabel?.font = UIFont.systemFont(ofSize: 16);
+        //iPad Mini 3
+        case "iPad4,4", "iPad4,5", "iPad4,6": optionButton.titleLabel?.font = UIFont.systemFont(ofSize: 16);
+        //iPad Mini 3
+        case "iPad4,7", "iPad4,8", "iPad4,9": optionButton.titleLabel?.font = UIFont.systemFont(ofSize: 16);
+        //iPad Mini 4
+        case "iPad5,1", "iPad5,2": optionButton.titleLabel?.font = UIFont.systemFont(ofSize: 16);
+        //iPad Pro
+        case "iPad6,3", "iPad6,4", "iPad6,7", "iPad6,8":optionButton.titleLabel?.font = UIFont.systemFont(ofSize: 23);
+        //Default
+        default: optionButton.titleLabel?.font = UIFont.systemFont(ofSize: 18);
+        }
+        
+        
+        
+        
+        
+        
         optionButton.setTitle(option, for: .normal)
         optionButton.setTitle(option, for: .highlighted)
         size(optionButton: optionButton)
--- WatsonDemo/GlobalConstants.swift
+++ WatsonDemo/GlobalConstants.swift
@@ -9,12 +9,20 @@
 import Foundation
 
 struct GlobalConstants {
-
     static let dennisNotoBluemixUsernameSTT = "8b772b77-c923-42ff-b2ef-29370cba1965"
     static let dennisNotoBluemixPasswordSTT = "NP0ooxJSFfL8"
     static let dennisNotoBluemixUsernameTTS = "833e48e0-2139-4142-935f-04190b698542"
     static let dennisNotoBluemixPasswordTTS = "DDxOcEN6TIRi"
     static let dennisNotoWorkspaceID = "1549fa38-7160-4106-82ee-73796b21f3b8"
-    static let nodeRedWorkflowUrl = "http://Node-Workflow-Hub.mybluemix.net/mobileV2-1"
-
+    static let nodeRedWorkflowUrl = "http://nodered-v3.mybluemix.net/mobileV2-1"
+    
+    static let sriniCheedallaBluemixPassword = "Westfield12"
+    static let sriniCheedallaBluemixUsername = "scheeda@us.ibm.com"
+    static let sriniCheedallaWorkspaceID = "62fb22ec-7163-44a2-b8c9-afd725c234be"
+    static let sriniCheedallNodeRedWorkflowUrl = "http://Westfield-Prototype.mybluemix.net/mobileV2-1"
+    
+    static let etayluzBluemixUsername = "d5e5ca34-f670-4a7f-98c7-1643d83ecc1d"
+    static let etayluzBluemixPassword = "N8UAQQPXw10Z"
+    
+    
 }
