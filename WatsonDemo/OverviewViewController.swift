187c42c689c0e7e653fb614663e3eb27027ca3db
Dennis  Noto <dennisnoto@gmail.com>
2018-04-12 16:14:40 -0600

    changed service bob

--- WatsonDemo/OverviewViewController.swift
+++ WatsonDemo/OverviewViewController.swift
@@ -44,7 +44,7 @@ class OverviewViewController: UIViewController {
         #elseif WATSONASST
             overviewImageView.image = #imageLiteral(resourceName: "Overview-WatsonAsst")
        //     setupUsecaseButtons(withOptions: ["Insure Assist|hello insure Assist","Bank Assist|hello Bank Assist","Wealth Assist|hello Money Assist","Quicken Loans|hello Quicken Loans"])
-             setupUsecaseButtons(withOptions: ["Insure Assist|hello insure Assist","Bank Assist|hello Bank Assist","Wealth Assist|hello Money Assist","Health Assist|hello Health Assist","Service Assist|hello Service"])
+             setupUsecaseButtons(withOptions: ["Insure Assist|hello insure Assist","Bank Assist|hello Bank Assist","Wealth Assist|hello Money Assist","Health Assist|hello Health Assist","Service Assist|hello Bob"])
         #elseif WATSONMETASST
             overviewImageView.image = #imageLiteral(resourceName: "Overview-WatsonMetAsst")
         #elseif WATSONWHIRLASST
